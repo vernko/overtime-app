90e1b742bc047e84435276581c83ac50d3201816
Vern Kofford <cheow3@gmail.com>
2017-11-09 18:50:06 -0700

    Created show and new availablity for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
-    it 'can be reached successfully' do
-        visit posts_path
-        expect(page.status_code).to eq(200)
-    end
+    describe 'index' do
+        it 'can be reached successfully' do
+            visit posts_path
+            expect(page.status_code).to eq(200)
+        end
 
-    it 'has a title of Posts' do
-        visit posts_path
-        expect(page).to have_content(/Posts/)
+        it 'has a title of Posts' do
+            visit posts_path
+            expect(page).to have_content(/Posts/)
+        end
     end
-  end
+    
+    describe 'creation' do
+        it 'has a new form that can be reached' do
+            visit new_post_path
+            expect(page.status_code).to eq(200)
+        end
+
+        it 'can be created from new form page' do
+            visit_new_post_path
+
+            fill_in 'post[date]', with: Date.today
+            fill_in 'post[rationale]', with: "Some rationale"
+
+            click_on "Save"
+
+            expect(page).to have_content("Some rationale")
+        end
+    end
 end
