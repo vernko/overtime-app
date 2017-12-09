1ca26caac3890721f6136745d15f1fccd62fb2d1
Vern Kofford <cheow3@gmail.com>
2017-12-09 01:27:35 -0700

    Another update to product assest managment rules

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,20 +3,15 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.digest = true
   config.log_level = :debug
-  config.log_tags = [ :request_id ]
-  config.action_mailer.perform_caching = false
+  config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  # if ENV["RAILS_LOG_TO_STDOUT"].present?
-  #   logger           = ActiveSupport::Logger.new(STDOUT)
-  #   logger.formatter = config.log_formatter
-  #   config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  # end
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
-  config.action_mailer.default_url_options = { :host => 'vff-overtime.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'wlp-overtime.herokuapp.com' }
 end
